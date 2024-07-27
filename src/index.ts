9bb73a9f1026a4e7cae7871d2564ff302955ef6d
estecore <estecore@yandex.ru>
2024-07-27 14:52:36 +0400

    refactor(index.ts): create usercontrollers.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-import express, { Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-import { validationResult } from "express-validator";
-
 import { registerValidation } from "./validations/auth";
-import { UserModel } from "./models/User";
 
 import { checkAuth } from "./utils/checkAuth";
+
+import * as UserControllers from "./controllers/UserControllers";
 
 dotenv.config();
 
@@ -18,7 +15,6 @@ if (!process.env.MONGO_CONNECT) {
 }
 
 const PORT = process.env.PORT || 1337;
-const JWT_SECRET = process.env.JWT_SECRET || "secret123";
 const MONGO_CONNECT = process.env.MONGO_CONNECT;
 
 mongoose
@@ -30,115 +26,11 @@ const app = express();
 
 app.use(express.json());
 
-app.post(
-  "/auth/register",
-  registerValidation,
-  async (req: Request, res: Response) => {
-    try {
-      const errors = validationResult(req);
+app.post("/auth/register", registerValidation, UserControllers.register);
 
-      if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
-      }
+app.post("/auth/login", UserControllers.login);
 
-      const password = req.body.password;
-      const salt = await bcrypt.genSaltSync(10);
-      const hash = await bcrypt.hash(password, salt);
-
-      const doc = new UserModel({
-        fullName: req.body.fullName,
-        email: req.body.email,
-        passwordHash: hash,
-        avatarUrl: req.body.avatarUrl,
-      });
-
-      const user = await doc.save();
-
-      const token = jwt.sign(
-        {
-          _id: user._id,
-        },
-        JWT_SECRET,
-        {
-          expiresIn: "30d",
-        }
-      );
-
-      const { passwordHash, ...userData } = user.toObject();
-
-      res.json({ userData, token });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({
-        message: "Failed to register",
-      });
-    }
-  }
-);
-
-app.post("/auth/login", async (req: Request, res: Response) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
-
-    if (!user) {
-      return res.status(404).json({
-        message: "Error login",
-      });
-    }
-
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user.passwordHash
-    );
-
-    if (!isValidPass) {
-      return res.status(400).json({
-        message: "Error login",
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      JWT_SECRET,
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    const { passwordHash, ...userData } = user.toObject();
-
-    res.json({ userData, token });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Failed to login",
-    });
-  }
-});
-
-//  ================= TODO change any type ====================
-app.get("/auth/me", checkAuth, async (req: any, res: Response) => {
-  try {
-    const user = await UserModel.findById(req.userId);
-
-    if (!user) {
-      return res.status(404).json({
-        message: "User not found",
-      });
-    }
-
-    const { passwordHash, ...userData } = user.toObject();
-
-    res.json({ userData });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Failed to get user",
-    });
-  }
-});
+app.get("/auth/me", checkAuth, UserControllers.getMe);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
