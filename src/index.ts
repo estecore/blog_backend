b15a9c8f68528e6922c63ad69e7182d9a0a5dc37
estecore <estecore@yandex.ru>
2024-07-27 12:23:50 +0400

    feat(index.ts): create express app, add jwt

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("hello world");
+import express from "express";
+
+import jwt from "jsonwebtoken";
+
+const PORT: number = 1337;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      password: req.body.password,
+      fullName: "Karl",
+    },
+
+    "secret123"
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
